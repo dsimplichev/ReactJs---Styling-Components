7645358511d2684bd12daa163b755e39ce941673
dsimplichev <dsimplichev1948@gmail.com>
2024-02-12 16:38:50 +0000

    header styled

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -59,10 +59,10 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label invalid={emailNotValid}>Password</Label>
+          <Label invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
